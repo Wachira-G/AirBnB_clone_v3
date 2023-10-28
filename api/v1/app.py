0dfd519d77ff9a1f0a1f9a3c801b15253378df4a
Gabriel Wachira <gabbyk.com@gmail.com>
2023-10-28 22:58:21 +0300

    add CORS to app

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,15 @@ from flask import Flask, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 # def create_app():
-app = Flask(__name__, )
+app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# Create a CORS instance allowing /* for 0.0.0.0
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
@@ -26,7 +30,8 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     app.run(
-            host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', 5000),
-            threaded=True,
-            )
+        host=getenv("HBNB_API_HOST", "0.0.0.0"),
+        port=getenv("HBNB_API_PORT", 5000),
+        threaded=True,
+        debug=True,
+    )
