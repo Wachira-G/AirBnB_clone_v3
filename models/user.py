276cb88cd3bbd10fe1411ace7d6c55ae6c6a7169
Gabriel Wachira <gabbyk.com@gmail.com>
2023-10-30 21:14:02 +0300

    modify password update format for python 3.4: cant add in kwargs

--- models/user.py
+++ models/user.py
@@ -30,6 +30,7 @@ class User(BaseModel, Base):
         if "password" in kwargs:
             text_password = kwargs.pop("password")  # Remove from kwargs
             hashed_password = hashlib.md5(text_password.encode()).hexdigest()
-            super().__init__(*args, **kwargs, password=hashed_password)
+            super().__init__(*args, **kwargs
+            self.password = password=hashed_password
         else:
             super().__init__(*args, **kwargs)
